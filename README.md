3b2378ff68dfbeb400cd013d0e88ad2515a1e0ae
Valentin Delaye <jonesbusy@users.noreply.github.com>
2025-04-03 12:17:12 +0200

    Project init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Jobcacher S3 Extension plugin
+
+This plugin is an extension of the [jobcacher-plugin](https://plugins.jenkins.io/jobcacher/) that allows you to store the caches in S3 compliant storage
+
+This was extracted from `jobcacher` since this [PR](https://github.com/jenkinsci/jobcacher-plugin/pull/408)
+
+## CONTRIBUTING
+
+See [CONTRIBUTING](CONTRIBUTING.md)
+
+## LICENSE
+
+Licensed under MIT, see [LICENSE](LICENSE.md)
+
---
+++ src/test/java/plugins/itemstorage/DataMigrationTest.java
@@ -0,0 +1,45 @@
+package plugins.itemstorage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import jenkins.plugins.itemstorage.GlobalItemStorage;
+import jenkins.plugins.itemstorage.ItemStorage;
+import jenkins.plugins.itemstorage.s3.NonAWSS3ItemStorage;
+import jenkins.plugins.itemstorage.s3.S3ItemStorage;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+@WithJenkins
+class DataMigrationTest {
+
+    @Test
+    @LocalData
+    void shouldMigrateAwsData(JenkinsRule jenkins) {
+        ItemStorage<?> storage = GlobalItemStorage.get().getStorage();
+        assertThat(storage, is(notNullValue()));
+        S3ItemStorage s3ItemStorage = (S3ItemStorage) storage;
+        assertThat(s3ItemStorage.getBucketName(), is("bucket1"));
+        assertThat(s3ItemStorage.getRegion(), is("us-gov-west-1"));
+        assertThat(s3ItemStorage.getCredentialsId(), is("s3"));
+        assertThat(s3ItemStorage.getPrefix(), is("the-prefix/"));
+    }
+
+    @Test
+    @LocalData
+    void shouldMigrateNonAwsData(JenkinsRule jenkins) {
+        ItemStorage<?> storage = GlobalItemStorage.get().getStorage();
+        assertThat(storage, is(notNullValue()));
+        NonAWSS3ItemStorage s3ItemStorage = (NonAWSS3ItemStorage) storage;
+        assertThat(s3ItemStorage.getBucketName(), is("bucket1"));
+        assertThat(s3ItemStorage.getRegion(), is("eu-central-2"));
+        assertThat(s3ItemStorage.getCredentialsId(), is("s3"));
+        assertThat(s3ItemStorage.getPrefix(), is("the-prefix/"));
+        assertThat(s3ItemStorage.getEndpoint(), is("http://localhost:9000"));
+        assertThat(s3ItemStorage.getParallelDownloads(), is(true));
+        assertThat(s3ItemStorage.getPathStyleAccess(), is(true));
+    }
+}
